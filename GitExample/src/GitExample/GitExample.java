75487bd79884fb5c873518cd9dc8a8c084b89125
[MK] <[msuarezg1@miumg.edu.gt]>
2025-04-25 09:13:45 -0600

    Initial commit

---
+++ GitExample/src/GitExample/GitExample.java
@@ -0,0 +1,12 @@
+package GitExample;
+
+
+/**
+ * @author Mark Suarez
+ */
+public class GitExample {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");   
+    }
+
+}
